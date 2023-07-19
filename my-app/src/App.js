896845d740c99e168b5aef59d4fcaae0c7d6e823
Vladimir Kolosov <tr4nk.kol@gmail.com>
2023-07-19 11:15:42 +0300

    Реализация кода без JSX разметки

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,28 +4,6 @@ import { createElement } from 'react';
 
 const currentYear = new Date().getFullYear();
 
-// export const App = () => {
-// 	return (
-// 		<div className="App" /* Декларативный код */>
-// 			<header className="App-header" /* Декларативный код */>
-// 				<img src={logo} className="App-logo" alt="logo" /* Декларативный код */ />
-// 				<p> Edit src/App.js and save to {/* Декларативный код */}reload. </p>
-// 				<a
-// 					className="App-link" /* Декларативный код */
-// 					href="https://reactjs.org"
-// 					target="_blank"
-// 					rel="noopener noreferrer"
-// 				>
-// 					Learn React
-// 				</a>
-// 				<p>
-// 					Current Year: {currentYear} {/* Декларативный код */}
-// 				</p>
-// 			</header>
-// 		</div>
-// 	);
-// };
-
 export const App = () => {
 	return createElement(
 		'div',
